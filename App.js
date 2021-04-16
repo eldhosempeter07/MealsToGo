ba071c64dfc35709123baca549e28acb363141c8
Eldhose M Peter <eldhosempeter07@gmail.com>
2021-04-16 15:28:00 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Initial View</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
